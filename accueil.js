098e4425e40eac930e4da80d2f2bbca1fd10cadd
Muller572b <etienne.muller83@gmail.com>
2026-06-17 13:24:26 +0200

    Update accueil.js

--- accueil.js
+++ accueil.js
@@ -11,11 +11,14 @@ document.addEventListener('DOMContentLoaded', () => {
     const clientId = localStorage.getItem('v2i_client_id');
 
     // Répertoire local pour faire la correspondance et afficher le code Cosium
-    const listeMagasins = {
+     const listeMagasins = {
         "1": "DON", "2": "A36", "3": "LUP", "4": "BAB", "6": "LIS", "7": "A67",
-        "9": "A40", "10": "BAA", "12": "AOS", "16": "BFO", "18": "ILE", "22": "O2C",
+        "9": "A40", "10": "BAA", "11": "BOR", "12": "AOS", "16": "BFO", "18": "ILE", "22": "O2C",
         "23": "COR", "24": "PAA", "25": "PLU", "28": "BOB", "29": "ROC", "31": "LAR",       
-        "33": "33", "35": "OBP", "99": "TEST99", "ADMIN": "COSIUM2026"
+        "33": "CCA", "34": "COZ", "35": "OBP", "36": "CCB", "37": "CCF", "39": "OBR", "41": "CAO",
+        "42": "FAA","43": "FCA", "44": "FAL", "46": "BAO", "47": "POB", "48": "BOF", "49": "O2B",
+         "50": "ATS", "51": "OSM", "52": "OBB", "53": "ONA", "56": "OBS", "57": "OPM",
+         "58": "OBV", "59": "ATB", "60": "KBO", "62": "OBO","99": "TEST99", "ADMIN": "COSIUM2026"
     };
 
     // On récupère le code Cosium associé (ex: "BFO" pour le magasin "16")
